d56b2b5ba93f24eddc88651e79adb3514fea3e7a
Belinda Heliot <Belinda@Belindas-MacBook-Air.local>
2014-12-22 06:41:24 -0600

    From the bitfountain.io Swift course

---
+++ Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Bridging-Header.h
+//  ExchangeAGram
+//
+//  Created by Belinda Heliot on 12/15/14.
+//  Copyright (c) 2014 Belinda Heliot. All rights reserved.
+//
+
+#import <FacebookSDK/FacebookSDK.h>
---
+++ FeedCell.swift
@@ -0,0 +1,16 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram
+//
+//  Created by Belinda Heliot on 12/10/14.
+//  Copyright (c) 2014 Belinda Heliot. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+}
